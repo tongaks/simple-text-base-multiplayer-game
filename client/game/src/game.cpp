afe432d12d65f4766495a9a0cc28b3ff319297a8
tongaks <rrfrokuu@gmail.com>
2025-12-23 20:34:10 +0800

    game: added map border handling

--- client/game/src/game.cpp
+++ client/game/src/game.cpp
@@ -23,39 +23,70 @@ void PrintMap(int width, int height, int playerX, int playerY) {
 	} std::cout << "\n\n";
 }
 
-int PrintMovementSelection() {
+int PrintMovementSelection(int posX, int posY) {
 	std::cout << "1. Move right\t2. Move left\t3. Move up\t4. Move down\n";
-	std::cout << "> ";
+	printf("(%i, %i)> ", posX, posY);
 
 	int choice = 0;
 	std::cin >> choice;
 	return choice;
 }
 
+bool HandleBorder(int width, int height, int posX, int posY, int plane, int dir) {
+	// plane = 1 -> x plane
+	// plane = 2 -> x plane
+
+	// dir = 1 -> move left/down
+	// dir = 2 -> move right/up
+
+
+	// handle x plane
+	if (plane == 1) {
+		if (dir == 1) {
+			if (posX - 1 <= -1) return false; // handle left
+		} else if (dir == 2) {
+			// 2 is the border wall
+			if (posX + 1 >= width-2) return false; // handle right
+		}
+	} else if (plane == 2) { // handle y
+		if (dir == 1) {
+			if (posY - 1 <= -1) return false; // handle up
+		} else if (dir == 2) {
+			if (posY + 1 >= height) return false; // handle down
+		}
+	}
+
+	return true; // allow movement
+}
+
 int main(int argc, char const *argv[]) {
 	// std::vector<std::vector<int>> map;
 
-	int width = 30; 
-	int height = 10;
+	int width = 40; 
+	int height = 20;
 	int playerX = 2;
 	int playerY = 2;
 
-	PrintMap(30, 10, 2, 2);
+	PrintMap(width, height, playerX, playerY);
 
 	while (1) {
-		switch (PrintMovementSelection()) {
-		case 1:
-			playerX += 1;
-			break;
-		case 2:
-			playerX -= 1;
-			break;
+		switch (PrintMovementSelection(playerX, playerY)) {
+		case 1: // right
+			if (HandleBorder(width, height, playerX, playerY, 1, 2)) {
+				playerX += 1;
+			} break;
+		case 2: // left
+			if (HandleBorder(width, height, playerX, playerY, 1, 1)) {
+				playerX -= 1;
+			} break;
 		case 3:
-			playerY += 1;
-			break;
+			if (HandleBorder(width, height, playerX, playerY, 2, 1)) {
+				playerY -= 1;
+			} break;
 		case 4:
-			playerY -= 1;
-			break;
+			if (HandleBorder(width, height, playerX, playerY, 2, 2)) {
+				playerY += 1;
+			} break;
 		default:
 			break;
 		}
